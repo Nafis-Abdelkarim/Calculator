de16163d96d77bd866e68653a44157403c1bd4b9
Nafis-Abdelkarim <kazcrb@gmail.com>
2023-05-04 23:29:36 +0100

    Update in some components

--- src/App.js
+++ src/App.js
@@ -1,62 +1,9 @@
-import { useReducer } from "react"
-import DigitButton from "./DigitButton"
-import OperationButton from "./OperationButton"
-import "./style.css"
-import { act } from "@testing-library/react"
-
-const Actions = {
-  AddDigit: 'add-digit',
-  ChooseOperation: 'Choose-operation',
-  Clear: 'clear',
-  DeleteDigit: 'delete-digit',
-  Evaluate: 'evaluate'
-}
-
-//I've use a reducer ro manage all of state 
-function reducer(state, {type, payload}) {
-  switch(type) {
-    case Actions.AddDigit:
-      return {
-        ...state,
-        currentOperend: `${currentOperend || ""} ${payload.digit}`,
-      }
+import React, {Component} from "react"
+import "./App.css"
+class App extends Component {
+  render(){
+    return <div className="app">Test</div>
   }
-}
-
-function App(){
-  const [{currentOperend, previousOperend, operation}, dispatch] = useReducer(
-    reducer, 
-    {}
-  )
-
-  // dispatch({type: Actions.AddDigit, payload: {digit: 1}})
-  return(
-    <div className="calculator-grid">
-      <div className="output">
-        <div className="previous-operend">{previousOperend} {operation}</div>
-        <div className="current-operend">{currentOperend}</div>
-      </div>
-      <button className="span-two">AC</button>
-      <button>DEL</button>
-      {/* <DigitButton digit="/" dispatch={dispatch}/> */}
-      <OperationButton operation="/" dispatch={dispatch}/>
-      <DigitButton digit="1" dispatch={dispatch}/>
-      <DigitButton digit="2" dispatch={dispatch}/>
-      <DigitButton digit="3" dispatch={dispatch}/>
-      <OperationButton operation="*" dispatch={dispatch}/>
-      <DigitButton digit="4" dispatch={dispatch}/>
-      <DigitButton digit="5" dispatch={dispatch}/>
-      <DigitButton digit="6" dispatch={dispatch}/>
-      <OperationButton operation="-" dispatch={dispatch}/>
-      <DigitButton digit="7" dispatch={dispatch}/>
-      <DigitButton digit="8" dispatch={dispatch}/>
-      <DigitButton digit="9" dispatch={dispatch}/>
-      <OperationButton operation="+" dispatch={dispatch}/>
-      <DigitButton digit="." dispatch={dispatch}/>
-      <DigitButton digit="0" dispatch={dispatch}/>
-      <button className="span-two">=</button>
-    </div>
-  )
 }
 
 export default App
--- src/DigitButton.js
+++
@@ -1,6 +0,0 @@
-import {Actions} from './App'
-
-export default function DigitButton({ dispatch, digit}){
-    return <button onClick={()=>{dispatch({type: Actions.AddDigit, payload: {digit}})}}>
-        {digit}</button>
-}
