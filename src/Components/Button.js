0eb40a30d09a2d47b93111a239ef3083978ba104
Nafis-Abdelkarim <kazcrb@gmail.com>
2023-05-07 12:34:24 +0100

    Updated

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
 
-//function to check the value if it is an operator 
-//we use the function isNaN: Returns a Boolean value 
-//that indicates whether a value is the reserved value NaN (not a number).
 const isOperator = val => {
     return !isNaN(val) || val === "." || val === "=";
-}
+};
 
 export const Button = props => (
-    <div 
+    <div
     className={`button-wrapper ${
-            isOperator(props.children) ? null : "operator"
-        }`} 
+        isOperator(props.children) ? null : "operator"
+    }`}
+    onClick={() => props.handleClick(props.children)}
     >
     {props.children}
-</div>
+    </div>
 );
-
-export default Button;
